817bb1df8c1e7c3294683dc712f71f665cb9cb61
Ethan Saw <ethansaw@gmail.com>
2023-05-20 20:38:10 -0700

    updated sign in nad login page

--- stormhacks-react/src/pages/LogInPage.js
+++ stormhacks-react/src/pages/LogInPage.js
@@ -6,17 +6,25 @@ function LogInPage() {
     return (
         <body>
         <Card>
-            <header className={classes.logo}>Forum Login</header>
-            <br /><br />
+            <header className={classes.logo}>Login</header>
+            {/* <br /><br /> */}
             <form method='post' action='login.php'>
-                Username<br />
-                <input type='text' name='Username' required=''/><br /><br />
-                Password<br />
-                <input type='password' name='Password' required=''/><br /><br />
-                <input type='submit' value='Log In' />
+                <div className={classes.regulartext}>
+                    <text>Username</text>
+                </div>
+                <input className={classes.input} type='text' name='Username' required=''/><br /><br />
+                <div className={classes.regulartext}>
+                    <text>Password</text>
+                </div>
+                <input className={classes.input} type='password' name='Password' required=''/><br /><br />
+                {/* <input type='submit' value='Log In' /> */}
+                <div className={classes.actions}>
+                    <button>Sign In</button>
+                </div>
             </form>
         </Card>
         <Card>
+            <text className={classes.regulartext}>New User? </text>
             <Link className={classes.linkbutton} to='/signup'>Create an account</Link>
         </Card>
 	</body>
--- stormhacks-react/src/pages/SignUpPage.js
+++ stormhacks-react/src/pages/SignUpPage.js
@@ -1,22 +1,38 @@
+import Card from '../components/signupprompt/Card';
+import classes from './SignUpPage.module.css';
+
 function SignUpPage() {
     return (
         <body>
-		<h1>Forum Registration</h1>
-		
-		<form method='post' action='registerCheck.php'>
-			<h3>Create Your Forum Account</h3>
-			First Name<br />
-			<input type='text' name='txtFirstName' required=''/><br /><br />
-			Last Name<br />
-			<input type='text' name='txtLastName' required=''/><br /><br />
-			Birthday<br />
-			<input type='date' name='dateBirthday' required='' value='datBirthday'/><br /><br />
-			Username<br />
-			<input type='text' name='txtUsername' required=''/><br /><br />
-			Password<br />
-			<input type='password' name='Password' required=''/><br /><br />
-			<input type='submit' value='Sign Up' />
-		</form>
+		<Card>
+			<header className={classes.logo}>Create an Account</header>
+			
+			<form method='post' action='registerCheck.php'>
+				<div className={classes.regulartext}>
+                    <text>First Name</text>
+                </div>
+				<input className={classes.input} type='text' name='txtFirstName' required=''/><br /><br />
+				<div className={classes.regulartext}>
+                    <text>Last Name</text>
+                </div>
+				<input className={classes.input} type='text' name='txtLastName' required=''/><br /><br />
+				<div className={classes.regulartext}>
+                    <text>Birthday</text>
+                </div>
+				<input className={classes.datebirthday} type='date' name='dateBirthday' required='' value='datBirthday'/><br /><br />
+				<div className={classes.regulartext}>
+                    <text>Username</text>
+                </div>
+				<input className={classes.input} type='text' name='txtUsername' required=''/><br /><br />
+				<div className={classes.regulartext}>
+                    <text>Password</text>
+                </div>
+				<input className={classes.input} type='password' name='Password' required=''/><br /><br />
+				<div className={classes.actions}>
+                    <button>Sign Up</button>
+                </div>
+			</form>
+		</Card>
 	</body>);
 }
 
